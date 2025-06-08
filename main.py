6bc7fdef699e95e58b2eea5928b2eb97b7c4e23c
blueo <hossein.dehghanipour1998@gmail.com>
2025-06-08 14:39:58 +0200

    refactor: ignores pycaches

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from garbage_classifier import GarbageClassifier
 
 # Example usage
 if __name__ == "__main__":
-    detection_model = YOLOModel(model_path='./models/best.pt')
+    detection_model = YOLOModel(model_path='./models/final.pt')
     gc = GarbageClassifier(detection_model=detection_model)  # Using a generic model for testing
     gc.run_video_stream(video_source=0)  # Use 0 for default webcam, or your IP cam URL
